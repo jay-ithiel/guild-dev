6288178afe674a47b9dac23fc06365802c438283
Jay Hwang <jayhwang0121@gmail.com>
2017-07-03 16:29:57 -0700

    add styling to blog form

--- src/js/components/blogs/blog_form.jsx
+++ src/js/components/blogs/blog_form.jsx
@@ -36,20 +36,29 @@ class BlogForm extends React.Component {
             <div id='blog-form-container'>
                 <form id='blog-form' onSubmit={ this.handleSubmit }>
                     <input type='text'
+                        id='blog-title-input'
+                        className='blog-input'
                         onChange={ this.handleChange('title') }
                         value={ this.state.title }
                         placeholder='Title'
                     />
 
-                    <button id='add-img-btn' className='btn'>Add Image</button>
+                    <button id='add-img-btn' className='blog-input btn'>
+                        <img id='add-img-icon'
+                            src='https://res.cloudinary.com/ddgtwtbre/image/upload/v1499124357/cam-icon_cztsyy.png'
+                        />
+                        <h4 className='title-2'>Add Image</h4>
+                    </button>
 
-                    <input type='text'
+                    <textarea type='text'
+                        id='blog-body-input'
+                        className='blog-input'
                         onChange={ this.handleChange('body') }
                         value={ this.state.body }
                         placeholder='Body'
                     />
 
-                    <button>Post</button>
+                <button id='blog-submit' className='btn primary-btn blog-input'>Post Blog</button>
                 </form>
             </div>
         );
