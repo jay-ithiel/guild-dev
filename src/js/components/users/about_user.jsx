04d106595529784811bc09e2c937a8dfb6eeb783
Jay Hwang <jayhwang0121@gmail.com>
2017-07-10 10:25:53 -0700

    style blog show component

--- src/js/components/users/about_user.jsx
+++ src/js/components/users/about_user.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const AboutUser = ({ user }) => (
-    <div id='about-user' className=''>
-        About User
+const AboutUser = props => (
+    <div id='about-user' className='flex'>
+        <div>
+            <img src={ props.currentUser.image[0].contentUrl } id='about-user-img' alt='Blog Image' />
+            <h4 id='about-user-name'>
+                User Name
+                {/* props.currentUser.name */}
+            </h4>
+        </div>
+
+        <div>
+            <p id='about-user-bio'>
+                Hello World! This is the user's intro/bio. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+        </div>
     </div>
 );
 
-export default AboutUser;
+const mapStateToProps = state => ({
+    currentUser: state.session.currentUser
+});
+
+export default connect(mapStateToProps, null)(AboutUser);
