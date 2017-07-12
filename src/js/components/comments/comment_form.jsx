1f48cbfeb97447ff56239f9b6c9693a305cee46d
Jay Hwang <jayhwang0121@gmail.com>
2017-07-11 17:50:30 -0700

    style comment form

--- src/js/components/comments/comment_form.jsx
+++ src/js/components/comments/comment_form.jsx
@@ -31,57 +31,24 @@ class CommentForm extends React.Component {
 
     render() {
         return (
-            <div>
-                <div className='blog-content desktop'>
-                    <form className='comment-form' onSubmit={ this.handleSubmit }>
-                        <label className='label'>
-                            <h4>COMMENT</h4>
-                            <textarea className='comment-input comment-body-input'
-                                onChange={ this.handleChange('body') }
-                                value={ this.state.body } />
-                        </label>
+            <div id='comment-form-container'>
+                <form onSubmit={ this.handleSubmit }>
+                    <textarea
+                        id='comment-input'
+                        onChange={ this.handleChange('body') }
+                        value={ this.state.body }
+                        placeholder='Your comment here...'
+                    />
 
-                        <label className='label'>
-                            <h4>NAME<span className='required'>*</span></h4>
-                            <input type='text'
-                                className='comment-input'
-                                onChange={ this.handleChange('author_name') }
-                                value={ this.state.author_name } />
-                        </label>
-
-                        <label className='label'>
-                            <h4>EMAIL<span className='required'>*</span></h4>
-                            <input type='text'
-                                className='comment-input'
-                                onChange={ this.handleChange('author_email') }
-                                value={ this.state.author_email } />
-                        </label>
-
-                        <button className='btn submit-btn'>POST COMMENT</button>
-                    </form>
-                </div>
+                <button id='submit-comment-btn' className='btn primary-btn'>
+                        Post Comment
+                    </button>
+                </form>
             </div>
         );
-        //
-        // return (
-        //     <div id='comment-form-container' className='blog-content'>
-        //         <form onSubmit={ this.handleSubmit }>
-        //             <textarea
-        //                 id='comment-input'
-        //                 className=''
-        //                 onChange={ this.handleChange('body') }
-        //                 value={ this.state.body }
-        //                 placeholder='Your comment here...'
-        //             />
-        //
-        //             <button id='submit-comment-btn' className='btn'>
-        //                 Post Comment
-        //             </button>
-        //         </form>
-        //     </div>
-        // );
     }
 }
+
 
 const mapStateToProps = state => ({
 
