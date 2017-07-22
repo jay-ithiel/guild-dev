a2e8da8fe72cb463fddf9611492c7e48d16bbd4d
Jay Hwang <jayhwang0121@gmail.com>
2017-07-22 16:10:56 -0700

    restyle about user component

--- src/js/components/users/about_user.jsx
+++ src/js/components/users/about_user.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
 import UserSVG from 'react-icons/lib/fa/user';
 
 const AboutUser = props => {
@@ -8,17 +9,16 @@ const AboutUser = props => {
     return !user ? <div></div> : (
         <div id='about-user' className='flex'>
             <div>
-                <div id='about-user-img'>
-                    <UserSVG size={ 90 }/>
-                </div>
-                <h4 id='about-user-name'>
-                    { user.profile.givenName } { user.profile.familyName }
-                </h4>
+                <div id='about-user-img'></div>
             </div>
 
             <div>
+                <h4 id='about-user-name'>
+                    {/* { user.profile.givenName } { user.profile.familyName } */}
+                    Lorem Ipsum
+                </h4>
                 <p id='about-user-bio'>
-                    Hello World! This is the user's intro/bio. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
             </div>
         </div>
