aee7568e1c3ce964ccb4d28f1a051365baa336e2
Jay Hwang <jayhwang0121@gmail.com>
2017-07-25 22:10:43 -0700

    change typo in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/js/Guild.js',
+  entry: './src/js/guild.js',
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'public'),
